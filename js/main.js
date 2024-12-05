b6dcb87a24b82868f60f10b59a2bdda4f0899dba
stas662 <92520407+stas662@users.noreply.github.com>
2024-12-05 21:59:40 +0200

    Added switching points

--- js/main.js
+++ js/main.js
@@ -1,18 +1,32 @@
 function startSlides(activeSlide) {
     const slides = document.querySelectorAll('.slide');
+    const dots = document.querySelectorAll('.dot');
 
     slides[activeSlide].classList.add('active');
+    dots[activeSlide].classList.add('activeDot');
 
-    for (const slide of slides) {
+    slides.forEach(function(slide, i) {
         slide.addEventListener('click', () => {
             clearActiveClasses();
             slide.classList.add('active')
+            dots[i].classList.add('activeDot')
         });
-    }
+    })
+
+    dots.forEach(function(dot, i) {
+        dot.addEventListener('click', () => {
+            clearActiveClasses();
+            dot.classList.add('activeDot')
+            slides[i].classList.add('active')
+        });
+    })
 
     function clearActiveClasses() {
         slides.forEach((slide) => {
             slide.classList.remove('active');
+        })
+        dots.forEach((dot) => {
+            dot.classList.remove('activeDot');
         })
     }
 }
