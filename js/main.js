dfe8bcf85b1d56dad868f566c0cede47bc7b1adb
stas662 <Makarov.Stas.2018@gmail.com>
2024-12-06 21:02:23 +0200

    Correction of unnecessary actions

--- js/main.js
+++ js/main.js
@@ -1,32 +1,18 @@
 function startSlides(activeSlide) {
     const slides = document.querySelectorAll('.slide');
-    const dots = document.querySelectorAll('.dot');
 
     slides[activeSlide].classList.add('active');
-    dots[activeSlide].classList.add('activeDot');
 
     slides.forEach(function(slide, i) {
         slide.addEventListener('click', () => {
             clearActiveClasses();
             slide.classList.add('active')
-            dots[i].classList.add('activeDot')
-        });
-    })
-
-    dots.forEach(function(dot, i) {
-        dot.addEventListener('click', () => {
-            clearActiveClasses();
-            dot.classList.add('activeDot')
-            slides[i].classList.add('active')
         });
     })
 
     function clearActiveClasses() {
         slides.forEach((slide) => {
             slide.classList.remove('active');
-        })
-        dots.forEach((dot) => {
-            dot.classList.remove('activeDot');
         })
     }
 }
